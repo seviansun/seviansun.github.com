8fce216c4e90373b8af6d5caa95a4fed1dd968cc
seviansun <sunyun1203@gmail.com>
2012-12-24 17:46:58 +0800

    summary

--- data/sitemap.js
+++ data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k)])]);}; 
-var b="rootNodes",c="pageName",d="好孕记",e="type",f="Wireframe",g="url",h="好孕记.html",i="children",j="好孕记0.3",k="好孕记0_3.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k,i,[]),_(c,l,e,f,g,m)])]);}; 
+var b="rootNodes",c="pageName",d="我的篱笆 显示回复内容概要PRD",e="type",f="Wireframe",g="url",h="我的篱笆_显示回复内容概要PRD.html",i="children",j="个人中心",k="个人中心.html",l="我的篱笆",m="我的篱笆.html";
 return _creator();
 })();
