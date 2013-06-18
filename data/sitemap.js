7135b874d16bd8854ba0d017e0a241edd4eec3b4
seviansun <sunyun1203@gmail.com>
2013-06-18 15:05:50 +0800

    accordian
    accordian

--- data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y)]),_(c,z,e,f,g,A,k,[_(c,l,e,f,g,B),_(c,n,e,f,g,C),_(c,p,e,f,g,D),_(c,r,e,f,g,E),_(c,F,e,f,g,G),_(c,v,e,f,g,H),_(c,x,e,f,g,I)]),_(c,J,e,f,g,K,k,[_(c,L,e,f,g,M),_(c,N,e,f,g,O)])]);}; 
-var b="rootNodes",c="pageName",d="站点地图",e="type",f="Wireframe",g="url",h="站点地图.html",i="首页",j="首页.html",k="children",l="Java",m="Java.html",n="C#",o="C_.html",p="IT前沿资讯",q="IT前沿资讯.html",r="视频播放主页面",s="视频播放主页面.html",t="IT前沿资讯页面",u="IT前沿资讯页面.html",v="搜索页面",w="搜索页面.html",x="视频TAG",y="视频TAG.html",z="首页(登录)",A="首页_登录_.html",B="Java_1.html",C="C__1.html",D="IT前沿资讯_1.html",E="视频播放主页面_1.html",F="IT前沿资讯主页面",G="IT前沿资讯主页面.html",H="搜索页面_1.html",I="视频TAG_1.html",J="用户中心",K="用户中心.html",L="新用户注册",M="新用户注册.html",N="用户个人主页",O="用户个人主页.html";
-return _creator();
-})();
